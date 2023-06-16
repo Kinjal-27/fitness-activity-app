3ba94d2f6dc472b7efc9f553c61282fa2befbdaf
Kinjal-27 <kinjal.bosamiya@scaletech.xyz>
2023-06-16 14:45:05 +0530

    Sidenav and Topheader design done

--- src/hoc/layout/layout.tsx
+++ src/hoc/layout/layout.tsx
@@ -1,8 +1,11 @@
 import { PropsWithChildren } from 'react';
+import SideNav from 'shared/components/sidenav/sidenav';
+import TopHeader from 'shared/components/topHeader/topHeader';
 
 const Layout: React.FC<PropsWithChildren> = (props) => {
 	return (
-		<div id='wrapper'>
+		<div id='wrapper' className='flex'>
+			<SideNav />
 			<div id='page-wrapper' className='full--width'>
 				{props.children}
 			</div>
---
+++ src/shared/components/sidenav/sidenav.tsx
@@ -0,0 +1,65 @@
+import { FC, useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+
+import { Dashboard, Logo, SideNavIcon } from '../icons/icons';
+import LogoImg from 'assets/images/logo.png';
+
+interface IIconProps {
+	width?: string;
+	height?: string;
+	className?: string;
+}
+
+interface ISideNavOpt {
+	SvgIcon: FC<IIconProps>;
+	urlLink: string;
+	title: string;
+}
+
+const SIDE_NAV_OPTIONS: ISideNavOpt[] = [
+	{
+		SvgIcon: Dashboard,
+		urlLink: '',
+		title: 'Home'
+	}
+];
+
+const SideNav = () => {
+	const [sidebarOpen, setSideBarOpen] = useState(false);
+	const location = useLocation();
+	const activeMenu = location.pathname.split('/')[1];
+
+	return (
+		<nav className='navbar-default' role='navigation'>
+			<div className={`navbar-static-side bg--white d-flex flex-column full--height collapsed`}>
+				<div className='title-logo pl--10 pt--15 pb--15 flex align-items--center'>
+					<NavLink to='/dashboard'>
+						<Logo />
+					</NavLink>
+					<p className='app-title'>FITNESS</p>
+				</div>
+				{/* <div className='collapse-icon' onClick={() => setSideBarOpen(!sidebarOpen)}>
+					<SideNavIcon className='cursor-pointer sidenav-icon' />
+				</div> */}
+				<div className='cursor-pointer d-flex flex-column icons'>
+					{SIDE_NAV_OPTIONS.map(({ SvgIcon, urlLink, title }: ISideNavOpt, index: number) => {
+						return (
+							<NavLink
+								to={urlLink}
+								key={index}
+								className={`nav-link ${activeMenu === urlLink && 'active-menu'}`}
+							>
+								<div className='nav-link-content d-flex full--width align-items-center bg--twilight-blue '>
+									<div>{<SvgIcon />}</div>
+									<p className='menu-label font--18px hide no-margin'>{title}</p>
+								</div>
+							</NavLink>
+						);
+					})}
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default SideNav;
