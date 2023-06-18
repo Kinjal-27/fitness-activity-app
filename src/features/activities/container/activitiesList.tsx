634727058c2bdd4bfb6125b737c139009c938ab0
Kinjal-27 <kinjal.bosamiya@scaletech.xyz>
2023-06-19 02:08:55 +0530

    align center to activity

--- src/features/activities/container/activitiesList.tsx
+++ src/features/activities/container/activitiesList.tsx
@@ -150,7 +150,7 @@ const ActivitiesList = () => {
 										<div className='back-arrow cursor--pointer' onClick={() => navigate(-1)}>
 											<BackArrowIcon className='back-arrow-svg' />
 										</div>
-										<div className='flex'>
+										<div className='flex align-items--center'>
 											<div
 												className='img-wrapper'
 												style={{
